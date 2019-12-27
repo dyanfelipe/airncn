19fd476a670e82601146aa183de08814ef8283a8
dyanfelipe <dyansilva@outlook.com>
2019-12-27 08:55:36 -0300

    acesso a aplicação

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const app = express();
@@ -17,8 +19,8 @@ let host =
 
 mongoose.connect(host, { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
 app.listen(process.env.PORT);
